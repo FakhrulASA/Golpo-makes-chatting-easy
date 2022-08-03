6407d3b4ada190ba5aa7634214a58082791a022f
Fakhrul Alam Siddiqei <fakhrulasa@gmail.com>
2022-08-03 20:06:09 +0600

    login page

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -12,14 +12,40 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Material(
       child: SafeArea(
-          child: Column(
-        children: [
-          Image.asset("assets/login.png", height: 300, fit: BoxFit.cover),
-          Text(
-            "Let's login",
-            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-          )
-        ],
+          child: Container(
+        margin: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Image.asset("assets/login.png",
+                  height: 100, fit: BoxFit.cover),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: const Text(
+                "Welcome back,\nLogin now ",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Align(
+              alignment: Alignment.topLeft,
+              child: const Text(
+                "If you are new, CREATE ACCOUNT",
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        ),
       )),
     );
   }
