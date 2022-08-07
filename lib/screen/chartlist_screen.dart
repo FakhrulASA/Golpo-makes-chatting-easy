c64282a410b27f144061de796f5948502b84892c
Fakhrul Alam Siddiqei <fakhrulasa@gmail.com>
2022-08-07 09:52:41 +0600

    - online status

--- lib/screen/chartlist_screen.dart
+++ lib/screen/chartlist_screen.dart
@@ -63,14 +63,18 @@ class _ChatListPageState extends State<ChatListPage> {
                 hintText: "Search here",
                 hintStyle: TextStyle(fontWeight: FontWeight.w200)),
           ),
+          SizedBox(
+            height: 10,
+          ),
           Flexible(
             child: ListView.builder(
-                itemCount: 20,
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemBuilder: (BuildContext context, int position) {
-                  return getRow(position);
-                }),
+              itemCount: 20,
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemBuilder: (BuildContext context, int position) {
+                return getRow(position);
+              },
+            ),
           )
         ],
       ),
@@ -78,45 +82,91 @@ class _ChatListPageState extends State<ChatListPage> {
   }
 
   Widget getRow(int i) {
+    bool isOnline;
+    if (i % 2 == 0) {
+      isOnline = true;
+    } else {
+      isOnline = false;
+    }
     return InkWell(
       onTap: () {
         Navigator.pushNamed(context, ApplicationRoute.messageDetailRoute);
       },
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      child: Column(
         children: [
-          CircleAvatar(
-              radius: (30),
-              backgroundColor: Colors.white,
-              child: Image.asset("assets/login.png")),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                    "User User",
-                    style: TextStyle(fontWeight: FontWeight.w600),
-                  ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                width: 12,
+              ),
+              CircleAvatar(
+                radius: 26,
+                backgroundColor: Color.fromARGB(255, 0, 172, 6),
+                child: CircleAvatar(
+                    radius: isOnline ? 22 : 26,
+                    backgroundColor: Colors.white,
+                    backgroundImage: AssetImage("assets/avatar.png")),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: const [
+                        SizedBox(
+                          child: Text(
+                            "User User",
+                            style: TextStyle(fontWeight: FontWeight.w600),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 100,
+                          child: Align(
+                            alignment: Alignment.topRight,
+                            child: Padding(
+                              padding: EdgeInsets.fromLTRB(0, 0, 12, 0),
+                              child: Text(
+                                "1:30 PM",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w300,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                                textAlign: TextAlign.left,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    // ignore: prefer_const_constructors
+                    Visibility(
+                      visible: true,
+                      child: const SizedBox(
+                        width: double.infinity,
+                        child: Text(
+                          "Message Message Message Message Messagge MMage Message ",
+                          textAlign: TextAlign.start,
+                          softWrap: false,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-                SizedBox(
-                  height: 5,
-                ),
-                SizedBox(
-                  width: double.infinity,
-                  child: Text(
-                    "Message Message Message Message Message Message Message Message Message Message Message Message MMessage Message MMessage Message Message Message Message Message Message Message ",
-                    textAlign: TextAlign.start,
-                    softWrap: false,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                  ),
-                )
-              ],
-            ),
-          )
+              )
+            ],
+          ),
+          const Divider()
         ],
       ),
     );
